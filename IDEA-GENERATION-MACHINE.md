438cb365bd37482a46c6f37fc5ca9901e0769da0
Quinn T <Quinn.GitHub@gmail.com>
2023-02-23 00:49:36 -0600

    stuff changed: look at diff
    slfhsfkljhsldfjhlsdhjflsjhdflkjhsdflkjhsdlkfhsdfh

---
+++ IDEA-GENERATION-MACHINE.md
@@ -0,0 +1,6 @@
+# A list of ideas
+
+The app will work just like SBDBG, except with more UI/UX stuff that makes it better, but also
+keep the keyboard-only a preferable option.
+
+Yea
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<h1>Supibot Visual Debugger</h1>
+
+<p>Yep.</p>
+
+<footer>Inspired by <a href="https://kotmisia.pl/sbdbg/">SBDBG</a> (<a href="https://git.kotmisia.pl/mm2pl/sbdbg">git</a>) by Mm2pl</footer>
+
